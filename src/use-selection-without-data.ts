85cbed500b4d571ae93f30d269fb1bb765a61b88
Igor Soloydenko <soloydenko@gmail.com>
2020-06-07 18:47:30 -0700

    fix: ensure callback is only ran once in use-selection-without-data

--- src/use-selection-without-data.ts
+++ src/use-selection-without-data.ts
@@ -1,15 +1,18 @@
 import { select, Selection } from 'd3';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useSelectionWithoutData(
   svgRef: React.RefObject<SVGSVGElement>,
   callback: (selection: Selection<SVGSVGElement, unknown, null, undefined>) => void,
 ): void {
+  const [processed, setProcessed] = useState(false);
   useEffect(
     () => {
       const svgElement = svgRef.current;
-      if (svgElement)
+      if (svgElement && !processed) {
+        setProcessed(true);
         callback(select(svgElement));
+      }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [svgRef, svgRef.current],
